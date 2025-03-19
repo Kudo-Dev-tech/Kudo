d2a4b944688959499ed1a0b59335674dd83e570f
AProject-agent <AProjectAgent@gmail.com>
2025-03-19 19:10:01 +0700

    Add comments and events

--- kudo-contracts/src/CovenantEvaluator.sol
+++ kudo-contracts/src/CovenantEvaluator.sol
@@ -9,20 +9,28 @@ import {
 } from "openzeppelin-contracts/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
 
 contract CovenantEvaluator is AccessControlDefaultAdminRules {
+    /// @notice Minimum number of approvals required for an evaluation to be valid
     uint256 private s_minApproval;
 
-    uint256 s_baseSlashPercentage;
+    /// @notice Base percentage of funds to be slashed for incorrect evaluation
+    uint256 private s_baseSlashPercentage;
 
-    uint256 s_baseSuspendTime;
+    /// @notice Base suspension time (in seconds) for incorrect evaluation
+    uint256 private s_baseSuspendTime;
 
-    uint256 s_maxConsecutiveInvalidAnswer;
+    /// @notice Maximum allowed consecutive invalid answers before permanent suspension
+    uint256 private s_maxConsecutiveInvalidAnswer;
 
-    uint256 s_slashedFund;
+    /// @notice Total amount of slashed funds collected from incorrect evaluation
+    uint256 private s_slashedFund;
 
-    uint256 s_minimumStakingBalance;
+    /// @notice Minimum staking balance required to evaluate
+    uint256 private s_minimumStakingBalance;
 
+    /// @notice ERC20 token used for staking
     IERC20 immutable i_stakingAsset;
 
+    /// @notice CovenantNFT contract
     CovenantNFT immutable i_cNFT;
 
     mapping(uint256 nftId => CovenantEvaluations covenantEvalutions) s_nftIdToCovenantEvaluations;
@@ -35,34 +43,67 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
     /// @param evaluator address of the whitelisted evaluator
     event EvaluatorWhitelisted(address evaluator);
 
+    /// @notice Emitted when evaluator is removed from whitelist
+    /// @param evaluator address of the removed evaluator
+    event EvaluatorRemoved(address evaluator);
+
+    /// @notice Emitted when an evaluator's suspension is lifted
+    /// @param evaluator Evaluator whose suspension has been removed
+    event SuspensionLifted(address evaluator);
+
+    /// @notice Emitted when slashed funds are claimed
+    /// @param to The address receiving the claimed funds
+    /// @param amount The amount of claimed funds
+    event SlashFundClaimed(address to, uint256 amount);
+
     /// @notice Thrown when the caller is not an authorized agent
     error AccessForbidden();
 
+    /// @notice Thrown when evaluator staking balance is insufficient
     error InsufficientStakingBalance();
 
+    /// @notice Thrown when the evaluator is suspended
     error Suspended();
 
     /// @notice Thrown when task has already been evaluated
     error TaskHasBeenEvaluated();
 
+    /// @notice Emitted when an evaluator stakes their tokens
+    /// @param evaluator The address of the evaluator
+    /// @param amount The amount of tokens staked
     event EvaluatorStaked(address indexed evaluator, uint256 amount);
 
+    /// @notice Emitted when the maximum consecutive invalid answers limit is set
+    /// @param maxConsecutiveInvalidAnswer The new maximum number of consecutive invalid answers allowed
+    event MaxConsecutiveInvalidAnswerSet(uint256 maxConsecutiveInvalidAnswer);
+
+    /// @notice Stores evaluation details of a cNFT
     struct CovenantEvaluations {
+        /// @notice Total amount of evaluation of the cNFT
         uint128 voteAmt;
+        /// @notice List of detailed evaluations made by evaluators
         EvaluationDetail[] evaluationsDetail;
     }
 
+    /// @notice Details of an evaluation made by an evaluator
     struct EvaluationDetail {
+        /// @notice Address of the evaluator submitting the evaluation
         address evaluator;
+        /// @notice Evaluator's answer
         bool answer;
+        /// @notice Raw encoded answer provided by the evaluator to prevent lazy evaluator
         bytes32 rawAnswer;
     }
 
+    /// @notice Stores details about an evaluator
     struct EvaluatorDetail {
+        /// @notice Evaluator is whitelist status
         bool whitelistStatus;
+        /// @notice Number of consecutive invalid answers given by the evaluator
         uint128 consecutiveInvalidAnswer;
-        /// @notice Evaluator staking balance
+        /// @notice The staking balance of the evaluator
         uint256 stakeBalance;
+        /// @notice Time until the evaluator is suspension is lifted
         uint256 suspendedUntil;
     }
 
@@ -86,6 +127,8 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         s_minimumStakingBalance = minimumStakingBalance;
     }
 
+    /// @notice Allows evaluator to stake a specified amount of tokens
+    /// @param amount The amount of tokens to be staked
     function stake(uint256 amount) public {
         if (!s_evaluatorDetails[msg.sender].whitelistStatus) revert AccessForbidden();
 
@@ -95,32 +138,53 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         emit EvaluatorStaked(msg.sender, amount);
     }
 
+    /// @notice Sets the maximum number of consecutive invalid answers allowed
+    /// @param maxConsecutiveInvalidAnswer The new maximum consecutive invalid answers allowed
     function setMaxConsecutiveInvalidAnswer(uint256 maxConsecutiveInvalidAnswer)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
         s_maxConsecutiveInvalidAnswer = maxConsecutiveInvalidAnswer;
+
+        emit MaxConsecutiveInvalidAnswerSet(maxConsecutiveInvalidAnswer);
     }
 
+    /// @notice Whitelists an evaluator
+    /// @param evaluator The address of the evaluator to be whitelisted
     function whitelistEvaluator(address evaluator) external onlyRole(DEFAULT_ADMIN_ROLE) {
         s_evaluatorDetails[evaluator].whitelistStatus = true;
 
         emit EvaluatorWhitelisted(evaluator);
     }
 
+    /// @notice Removes an evaluator from the whitelist
+    /// @param evaluator Evaluator that will be removed
     function removeEvaluator(address evaluator) external onlyRole(DEFAULT_ADMIN_ROLE) {
         delete s_evaluatorDetails[evaluator].whitelistStatus;
+
+        emit EvaluatorRemoved(evaluator);
     }
 
+    /// @notice Claim funds to a specified address
+    /// @param to The address that will receive the claimed funds
     function claimSlashedFund(address to) external onlyRole(DEFAULT_ADMIN_ROLE) {
         i_stakingAsset.transfer(to, s_slashedFund);
+        emit SlashFundClaimed(to, s_slashedFund);
+
         delete s_slashedFund;
     }
 
+    /// @notice Lifts the suspension of a specified evaluator
+    /// @param evaluator The evaluator which the suspension will be removed
     function removeEvaluatorSuspension(address evaluator) external onlyRole(DEFAULT_ADMIN_ROLE) {
         delete s_evaluatorDetails[evaluator].suspendedUntil;
+
+        emit SuspensionLifted(evaluator);
     }
 
+    /// @notice Submits an evaluation for Covenant NFT
+    /// @param nftId The ID of the NFT being evaluated
+    /// @param answer The evaluator’s response, encoded as a bytes32 value
     function evaluate(uint256 nftId, bytes32 answer) external activeEvaluator(msg.sender) {
         if (s_nftIdToEvaluatorVoteStatus[nftId][msg.sender]) revert TaskHasBeenEvaluated();
 
@@ -137,10 +201,15 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         }
     }
 
+    /// @notice Retrieves details of a specific evaluator
+    /// @param evaluator The address of the desired evaluator
+    /// @return EvaluatorDetail Struct containing detailed information
     function getEvaluatorDetails(address evaluator) external view returns (EvaluatorDetail memory) {
         return s_evaluatorDetails[evaluator];
     }
 
+    /// @notice Extracts the encoded answers for a desired Covenant NFT
+    /// @param nftId The ID of the Covenant NFT whose answer is being extracted
     function _extractAnswer(uint256 nftId) internal {
         uint256 approvalAmt;
 
@@ -165,6 +234,9 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         }
     }
 
+    /// @notice Compares the extracted answer of the Covenant NFT with the majority vote
+    /// @param nftId The ID of the Covenant NFT answers that are being compared
+    /// @param majorityVote The majority decision of the evaluation
     function _compareAnswer(uint256 nftId, bool majorityVote) internal {
         for (uint256 i; i < s_nftIdToCovenantEvaluations[nftId].evaluationsDetail.length; ++i) {
             if (s_nftIdToCovenantEvaluations[nftId].evaluationsDetail[i].answer != majorityVote) {
@@ -180,6 +252,8 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         }
     }
 
+    /// @notice Handles actions for evaluator who provided an invalid answer
+    /// @param evaluator The address of the evaluator
     function _handleInvalidEvaluator(address evaluator) internal {
         ++s_evaluatorDetails[evaluator].consecutiveInvalidAnswer;
 
@@ -193,6 +267,9 @@ contract CovenantEvaluator is AccessControlDefaultAdminRules {
         s_evaluatorDetails[evaluator].suspendedUntil = block.timestamp + 365 days;
     }
 
+    /// @notice Function to create commitment to compare encoded evaluator answer
+    /// @param sender The address of the evaluator
+    /// @param answer The evaluator’s expected answer
     function _createCommitment(address sender, bool answer) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(sender, answer));
     }
