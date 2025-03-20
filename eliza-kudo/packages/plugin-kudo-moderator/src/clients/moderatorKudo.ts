dd569dd61c7f3691e443e510f34fcdad19e15d52
AI-Agent-D <vladimirzedong7@gmail.com>
2025-03-20 19:30:22 +0800

    Added createConfig

--- eliza-kudo/packages/plugin-kudo-moderator/src/clients/moderatorKudo.ts
+++ eliza-kudo/packages/plugin-kudo-moderator/src/clients/moderatorKudo.ts
@@ -1,29 +1,38 @@
 // Under Construction
 
+import { createConfig, http } from '@wagmi/core'
+import { arbitrum } from '@wagmi/core/chains'
 import { watchContractEvent } from '@wagmi/core'
 import { moderatorKudoABI } from "./moderatorKudoABI"
 import { Client, IAgentRuntime } from '@elizaos/core';
+import { type CreateConfigParameters } from '@wagmi/core'
 
+const config = createConfig({
+    chains: [arbitrum],
+    transports: {
+        [arbitrum.id]: http("https://arb-mainnet.g.alchemy.com/v2/Q5TxpTVf_JI2DtOAgZuy17s0Ln2HDrt7")
+    }
 
-/**
- * async watchEvent() {
-    const event = watchContractEvent(config,
-        {address: "0x530BEba1A237a01f342199Bf0d3FC5FE628e4cB8",
+})
+
+async function watchEvent() {
+    const unwatch = watchContractEvent(config, {
         moderatorKudoABI,
-        eventName: "SettlementDataSet"
+        args: { 
+          to: '0x530BEba1A237a01f342199Bf0d3FC5FE628e4cB8', 
+        }, 
+        onLogs(logs) {
+          console.log('watchEvent Triggered!')
         },
-        (type, name) => { // Obtain the covenant details
-            console.log("Evaluator Function Triggered");
-            const details = getCovenantDetails(type, name); //Need some help here - where to import
-            return details
-        }
-    )
+      })
+    console.log(unwatch)
 }
- */
+
+
 
 export const KudoModeratorClientInterface: Client = {
     start: async (runtime: IAgentRuntime) => {
-        console.log("Starting Kudo Moderator Client");
+        watchEvent()
         return null;
     },
     stop: async (_runtime: IAgentRuntime) => {
