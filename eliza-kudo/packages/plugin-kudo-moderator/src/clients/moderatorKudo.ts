312234a4195a54579318807c2b959e58f32cbaee
AI-Agent-D <vladimirzedong7@gmail.com>
2025-03-21 14:53:34 +0800

    Switched from WAGMI to VIEM

--- eliza-kudo/packages/plugin-kudo-moderator/src/clients/moderatorKudo.ts
+++ eliza-kudo/packages/plugin-kudo-moderator/src/clients/moderatorKudo.ts
@@ -1,34 +1,34 @@
 // Under Construction
 
-import { createConfig, http } from '@wagmi/core'
-import { arbitrum } from '@wagmi/core/chains'
-import { watchContractEvent } from '@wagmi/core'
+
 import { moderatorKudoABI } from "./moderatorKudoABI"
 import { Client, IAgentRuntime } from '@elizaos/core';
-import { type CreateConfigParameters } from '@wagmi/core'
+import { createPublicClient, http } from 'viem'
+import { arbitrum } from 'viem/chains'
 
-const config = createConfig({
-    chains: [arbitrum],
-    transports: {
-        [arbitrum.id]: http()
-    }
-
+const publicClient = createPublicClient({
+    chain: arbitrum,
+    transport: http("https://arb-mainnet.g.alchemy.com/v2/Q5TxpTVf_JI2DtOAgZuy17s0Ln2HDrt7")
 })
 
-async function watchEvent() {
-    const unwatch = watchContractEvent(config, {
-        abi: moderatorKudoABI,
+async function watchContractEvent() {
+    // Watch an Event
+
+    const watcher = publicClient.watchContractEvent({
         address: '0x530BEba1A237a01f342199Bf0d3FC5FE628e4cB8',
-        onLogs(logs) {
-          console.log('watchEvent Triggered!')
-        },
-      })
-    console.log(unwatch)
+        abi: moderatorKudoABI,
+        onLogs: logs => {
+            console.log('watchEvent Triggered!')
+          }
+    })
+
+    return "TRIGGERED"
 }
 
 export const KudoModeratorClientInterface: Client = {
     start: async (runtime: IAgentRuntime) => {
-        await watchEvent()
+        const data = await watchContractEvent()
+        console.log(data)
         return null;
     },
     stop: async (_runtime: IAgentRuntime) => {
